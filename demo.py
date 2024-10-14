f287edc0fafc1407562c684ec2d7d50f96bc2846
Arsen Nuramatov <anuramat@pm.me>
2024-10-14 07:38:22 +0200

    fix(demo): shebang

--- demo.py
+++ demo.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 """
 Copyright (C) 2018 NVIDIA Corporation.  All rights reserved.
 Licensed under the CC BY-NC-SA 4.0 license (https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode).
